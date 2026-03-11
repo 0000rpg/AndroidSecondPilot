26af80449e10f136706126ec128424773a24575b
0000rpg <cryr.userme@gmail.com>
2026-03-11 09:37:32 +0300

    First stable build

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <router-view />
 </template>
 
-<style scoped></style>
+<script setup>
+// Здесь может быть глобальная логика, если нужна
+</script>
+
+<style>
+/* Глобальные стили можно добавить здесь */
+</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,20 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 import router from './router'
-
 import { useAuthStore } from './stores/auth'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate)
 
 const app = createApp(App)
 
-app.use(createPinia())
-
 app.use(pinia)
-
 app.use(router)
 
-const authStore = useAuthStore()
-authStore.loadFromStorage()
-
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+// const authStore = useAuthStore()
+// authStore.loadFromStorage()
 
 app.mount('#app')
